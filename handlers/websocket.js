6cb3f77c8779e0dbaeff828acbee5e0227c8f95b
nikita-skobov <nskobov.websites@gmail.com>
2019-02-19 14:57:34 -0600

    websocket fail...

--- handlers/websocket.js
+++ handlers/websocket.js
@@ -1,12 +1,24 @@
 // eslint-disable-next-line
 'use strict';
 
+const AWS = require('aws-sdk')
+
+const ddb = new AWS.DynamoDB.DocumentClient({
+  region: process.env.REGION,
+})
+
 const SUCCESS = {
   statusCode: 200,
 }
 
-module.exports.connect = async (event, context) => {
+module.exports.connect = async (event, context, callback) => {
   console.log(event)
+  console.log('\n')
+
+  Object.keys(event).forEach((key) => {
+    console.log(`KEY: ${key}`)
+    console.log(event[key])
+  })
 
   return SUCCESS
 }
