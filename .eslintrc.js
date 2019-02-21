298d80cb19e449a4c6b146e01c88525ee65f0a7b
nikita-skobov <nskobov.websites@gmail.com>
2019-02-20 19:55:46 -0600

    adds eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  "extends": "airbnb",
+  // "plugins": [
+  //     "react",
+  //     "jsx-a11y",
+  //     "import"
+  // ],
+  "rules": {
+      "semi": [2, "never"],
+      "linebreak-style": ["off"],
+      "react/jsx-filename-extension": ["off"],
+      "react/jsx-one-expression-per-line": ["off"],
+      "no-console": ["off"]
+  },
+};
